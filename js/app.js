ef197a8b664f86d82f33d4bcdd7ec531239399b7
whfflapfhd <whfflapfhd@naver.com>
2021-03-05 18:05:10 +0900

    add vanilla script

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ Date.prototype.format = function(format){
 		switch($1){
 			case 'yyyy' : return _this.getFullYear();
 			case 'MM' : return ('0' + (1 + _this.getMonth())).slice(-2);
-			case 'dd' : return ('0' + _this.getDate()).slice(-2);
+			case 'dd' : return ('0' + (_this.getDate()-1)).slice(-2);
 			case 'hh' : return _this.getHours();
 			case 'mm' : return _this.getMinutes();
 			default: return $1;
@@ -36,7 +36,7 @@ function getPost(url){
 		dataType : 'json',
 		chche : false,
 		success : function(resp){
-			data = resp.data.posts;
+            data = resp.data.posts;
 		},
 		error : function(msg){
 			data = null;
@@ -48,9 +48,9 @@ function printPost(num){
 	$(".monitoring").hide();
 	$(".viewAll").show();
 	getPost(urlAddress+num);
-	var html = '';            
+	var html = '';
 	$.each(data,function(idx,value){
-		html += "<li><span>"+value.assets+"</span><a href='"+value.url+"' target='_blank'>"+value.text+"</a></li>";                
+		html += "<li><span>"+value.assets+"</span><a href='"+value.url+"' target='_blank'>"+value.text+"</a></li>";
 	});
 	$(".viewAll").html(html);
 };
@@ -60,7 +60,7 @@ function printNewPost(){
 	var html = '',
 	$currentCnt = 0;
 	$.each(data,function(idx,value){
-		var $postDate = value.start_date.split('T')[0].split('-').join('');                         
+		var $postDate = value.start_date.split('T')[0].split('-').join('');
 		if($today != $postDate) return false;
 		html += "<li class='new'><span>"+value.assets+"</span><a href='"+value.url+"' target='_blank'>"+value.text+"</a></li>";
 		$currentCnt = idx+1;
@@ -76,30 +76,49 @@ printNewPost();
 var post = setInterval(printNewPost, 10000);
 
 
-const Disclosure = {};
-const _disclosureUrl = 'https://project-team.upbit.com/api/v1/disclosure?region=kr&per_page=';        
-let _postCount = 0;
-Disclosure.$getData = function(requestUrl,callback){	
-	const xhr = new XMLHttpRequest();
-	xhr.open('GET', requestUrl);	
-	xhr.send();
-	xhr.onload = function(){								
-		if(xhr.status === 200){
-			callback(JSON.parse(xhr.responseText));
-		}else{
-			console.log('Error!');
-		};		
-	}
-}
-Disclosure.$getDate = function(range){
-	let $currentDate = new Date();
-	return $currentDate.format(range)
-}
-Disclosure.$today = Disclosure.$getDate('yyyy-MM-dd');
+const _disclosureUrl = 'https://project-team.upbit.com/api/v1/disclosure?region=kr&per_page=';
+const Disclosure = function(){
+    this.data = null;
+    this.getDate = function(range){
+        let currentDate = new Date();
+        return currentDate.format(range)
+    };
+    this.today = this.getDate('yyyy-MM-dd');
+    this.getData = function(num,callback){
+        let count = (num === undefined) ? 10 : num;
+        let requestUrl = _disclosureUrl + count;
+        const _this = this;
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', requestUrl);
+        xhr.send();
+        xhr.onload = function(){
+            if(xhr.status === 200){
+                callback( JSON.parse(xhr.responseText).data.posts);
+            }else{
+                console.log('Error!');
+            };
+        }
+    };
+    this.printPost = function(e){
+        let _this = this;
+        let printDataCount = e.target.dataset.count;
+        let className = e.target.dataset.classname || '';
+        let array;
+        let htmlNode = '';
 
-//sample
-Disclosure.$data = Disclosure.$getData(_disclosureUrl,function(result){
-	Disclosure.$data = result
-})
-console.log(Disclosure.$data); //undifind
+        this.getData(printDataCount,function(result){
+            array = result;
+            array.forEach(function(value,idx){
+                if(className === 'new' && value.start_date.split('T')[0] !== _this.today) return false
+                htmlNode += `<li class=${className}><span>${value.assets}</span><a href=${value.url} target="_blank">${value.text}</a></li>`;
+            });
+            document.querySelector(".monitoring").innerHTML = htmlNode;
+        });
+    }
+};
 
+const disclosure = new Disclosure();
+let btn = document.querySelector('.btn-box');
+btn.addEventListener('click',function(e){
+    disclosure.printPost(e)
+});
