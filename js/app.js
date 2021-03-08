c5d9c0ae8c633c97bddad4ca2af46ab9076cbc28
whfflapfhd <whfflapfhd@naver.com>
2021-03-08 18:11:59 +0900

    localStorage 적용

--- js/app.js
+++ js/app.js
@@ -1,31 +1,34 @@
+'use strict';
+
 /*Set Date Format*/
 Date.prototype.format = function(format){
 	if(!this.valueOf()) return '';
 	let _this = this;
-	return format.replace(/(yyyy|mm|dd|hh|mm)/gi,function($1){
+	return format.replace(/(yyyy|mm|dd|hh|mm|ss|ms)/gi,function($1){
 		switch($1){
 			case 'yyyy' : return _this.getFullYear();
 			case 'MM' : return ('0' + (1 + _this.getMonth())).slice(-2);
 			case 'dd' : return ('0' + _this.getDate()).slice(-2);
 			case 'hh' : return _this.getHours();
-			case 'mm' : return _this.getMinutes();
+			case 'mm' : return ('0' + _this.getMinutes()).slice(-2);
+            case 'ss' : return _this.getSeconds();
+            case 'ms' : return _this.getMilliseconds();
 			default: return $1;
 		}
 	});
 };
 
-const _disclosureUrl = 'https://project-team.upbit.com/api/v1/disclosure?region=kr&per_page=';
+const _disclosureUrl = 'https://project-team.upbit.com/api/v1/disclosure?region=kr&per_page=20';
 const Disclosure = function(){
-    this.data = null;
     this.getDate = function(range){
         let currentDate = new Date();
         return currentDate.format(range)
     };
     this.today = this.getDate('yyyy-MM-dd');
-	this.postId = [];
-    this.getData = function(num,callback){
-        let count = (num === undefined) ? 10 : num;
-        let requestUrl = _disclosureUrl + count;
+
+    /*Get Data*/
+    this.getData = function(callback){
+        let requestUrl = _disclosureUrl;
         const _this = this;
         const xhr = new XMLHttpRequest();
         xhr.open('GET', requestUrl);
@@ -34,39 +37,51 @@ const Disclosure = function(){
             if(xhr.status === 200){
 				let responcesData = JSON.parse(xhr.responseText).data.posts;
 				callback(responcesData);
-				/*if(_this.postId.length > 0 && _this.postId[0] === responcesData[0].id){
-					return false
-				}else{
-					callback(responcesData);
-				};*/
             }else{
                 console.log('Error!');
             };
         }
     };
-    this.printPost = function(e){		
+    this.printPost = function(e){
         let _this = this;
-        let printDataCount = e.target.dataset.count;
-        let className = e.target.dataset.classname || '';
+        let _target = (e) ? e.target : 'undefined';
+        let className;
         let array;
-        let htmlNode = '';		
-        this.getData(printDataCount,function(result){
-            array = result;			
-            array.forEach(function(value,idx){				
-                if(className === 'new' && value.start_date.split('T')[0] !== _this.today) return false
-				_this.postId.push(value.id);
+        let storageData;
+        let htmlNode = '';
+
+        this.getData(function(result){
+            array = result;
+            storageData = localStorage.getItem('data');
+            let currentBoolean = (storageData) ? JSON.parse(storageData)[0].id === array[0].id : false;
+            if(!currentBoolean){
+                localStorage.setItem('data' , JSON.stringify(array));
+                console.log('use xhr');
+            }else{
+                array = JSON.parse(storageData);
+                console.log('use local');
+            };
+            array.forEach(function(value,idx){
+                /*
+                if(className === 'new' && value.start_date.split('T')[0] !== _this.today){
+                    htmlNode = '<li class="nopost">데이터 없음</li>';
+                    return false
+                };
+                */
+                className = (value.start_date.split('T')[0] !== _this.today) ? '' : 'new';
                 htmlNode += `<li class=${className}><span>${value.assets}</span><a href=${value.url} target="_blank">${value.text}</a></li>`;
-            });						
-			if(_this.postId.length > 0){
-				document.querySelector(".monitoring").innerHTML = htmlNode;
-				console.log('DONE');
-			}			
+            });
+            _this.firstID = array[0].id;
+            document.querySelector(".monitoring").innerHTML = htmlNode;
         });
+
     }
 };
 
 const disclosure = new Disclosure();
 let btn = document.querySelector('.btn-box');
-btn.addEventListener('click',function(e){	
+let btnNew = btn.querySelectorAll('button')[0];
+btn.addEventListener('click',function(e){
     disclosure.printPost(e)
 });
+disclosure.printPost();
